ff21239f88ca733e093e19ec132e99fafafc98c0
jaroslaw <jarek.skrzypczak@o2.pl>
2020-01-20 21:14:33 +0100

    Make games list in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
+import chess.parser.pgn.PGNGame;
 import configuration.CommandParser;
 import configuration.Config;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         //todo parse command args
+        //test args src/main/resources/examples/<fileName> output.pgn
         Config config = new CommandParser().parseCommandArguments(args);
 
         //todo read file
         //todo parse file
+        PgnFileManager pgnFileManager = new PgnFileManager();
+        List<PGNGame> pgnGameList = pgnFileManager.parsePgnFile(config.getInputPath());
+
         //todo filters
         //todo save output file || (send to server -> receive result -> save output file)
     }
